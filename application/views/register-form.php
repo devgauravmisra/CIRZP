b1d87ad2c8b83a20197e7dba3e41461d8ab69bed
gaurav <gauravphpdev@razorpay.com>
2021-08-22 11:30:11 +0530

    added some file

--- application/views/register-form.php
+++ application/views/register-form.php
@@ -10,32 +10,38 @@
     <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
     <title>Razorpay</title>
 </head>
-<body>
+<body style="background-color: blue !important;">
 <div class="container">
     <div class="row">
+        <div class="col-sm-5 col-md-5 col-lg-5 mx-auto">
+           <h3 style="color:white !important; margin-top: 100px;">Razorpay  Payment Gateway Integration with Codeigniter Framework</h3>
+           
+        </div>
         <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
             <div class="card card-signin my-5">
                 <div class="card-body">
-                    <h5 class="card-title text-center">Razorpay Integration in CI3</h5>
+                    <h5 class="card-title text-center">Checkout</h5>
                     <form action="<?php echo base_url().'index.php/register/pay'; ?>" method="post" class="form-signin">
                         <div class="form-label-group">
                             <label for="name">Name <span style="color: #FF0000">*</span></label>
-                            <input type="text" name="name" id="name" class="form-control" placeholder="Name"  required >
+                            <input type="text" name="name" id="name" class="form-control" placeholder="Customer Name"  required >
                         </div> <br/>
                         <div class="form-label-group">
                             <label for="email">Email <span style="color: #FF0000">*</span></label>
-                            <input type="text" name="email" id="email" class="form-control" placeholder="Email address" required>
+                            <input type="text" name="email" id="email" class="form-control" placeholder="Customer Email address" required>
                         </div> <br/>
                         <label for="contact">Contact <span style="color: #FF0000">*</span></label>
                         <div class="form-label-group">
-                            <input type="text" id="contact" name="contact" class="form-control" placeholder="Contact" required>
+                            <input type="text" id="contact" name="contact" class="form-control" placeholder="Customer Contact No" required>
                         </div><br/>
           <label for="subject">Amount <span style="color: #FF0000">*</span></label>
                         <div class="form-label-group">
                             <input type="text" id="amount" name="amount"  class="form-control" placeholder="Amount" required>
                         </div><br/>
                        <br/>
-                        <button type="submit" name="sendMailBtn" class="btn btn-lg btn-primary btn-block text-uppercase" >Pay</button>
+                        <button type="submit" name="sendMailBtn" class="btn btn-lg btn-primary btn-block text-uppercase" >Pay with Razorpay</button>
+                        <spam> <img src="<?php echo base_url('image/gaurav.png'); ?>"  style="width:100px; height:100px;margin-left: 30%;"/></spam>
+
                     </form>
                 </div>
             </div>
